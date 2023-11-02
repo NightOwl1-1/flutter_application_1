b93fdd70de4f4fe03077d109b5b1f2cb3d7b6442
Richard Schmitz Riedo <97689387+NightOwl1-1@users.noreply.github.com>
2023-11-01 21:20:16 -0300

    Create score_card.dart

---
+++ lib/score_card.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ScoreCard extends StatelessWidget {
+  final int correctAnswers;
+  final int wrongAnswers;
+
+  ScoreCard({required this.correctAnswers, required this.wrongAnswers});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildScoreItem('Acertos', correctAnswers, Colors.green),
+        SizedBox(width: 20),
+        _buildScoreItem('Erros', wrongAnswers, Colors.red),
+      ],
+    );
+  }
+
+  Widget _buildScoreItem(String label, int score, Color color) {
+    return Column(
+      children: [
+        Text(
+          label,
+          style: TextStyle(fontSize: 18, color: color),
+        ),
+        Text(
+          score.toString(),
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: color,
+          ),
+        ),
+      ],
+    );
+  }
+}
